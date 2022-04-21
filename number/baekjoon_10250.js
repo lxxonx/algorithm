aceed7aa7a78a878c96f95ae16e13bcc619a5578
lxxonx <leeonechang92@gmail.com>
2022-04-21 23:33:18 +0900

    baekjoon 10250 fix

--- number/baekjoon_10250.js
+++ number/baekjoon_10250.js
@@ -2,12 +2,13 @@
 // const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const input = `2
-6 12 10
-30 50 72`.split("\n");
+6 12 14
+30 50 60`.split("\n");
 
 const t = parseInt(input[0]);
 
 for (let i = 1; i <= t; i++) {
   const [h, w, n] = input[i].split(" ").map(Number);
-  console.log(`${n % h}${Math.ceil(n / h) < 10 ? "0" : ""}${Math.ceil(n / h)}`);
+  const floor = n % h == 0 ? h : n % h;
+  console.log(`${floor}${Math.ceil(n / h) < 10 ? "0" : ""}${Math.ceil(n / h)}`);
 }
